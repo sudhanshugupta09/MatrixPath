988f90fe36009553be81d3373e40a44d85afdaf0
Sudhanshu Gupta <sudhanshu.gupta090@gmail.com>
2019-02-07 00:07:08 -0600

    Added BFS traversal and validation checks

--- solve.py
+++ solve.py
@@ -50,17 +50,33 @@ def path_exists(grid, queries):
         dfs breaks
     '''
 
+    def display_grid(grid):
+        for row in grid:
+            print(row)
 
-    def check_valid_neighbors(row, col, row_max, col_max):
+    def check_valid_neighbors(row, col, row_max, col_max, visited):
         row_check = [-1, 1, 0, 0]
         col_check = [0, 0, -1, 1]
         valid_neighbors = []
 
         for r ,c in zip(row_check, col_check):
             if row + r >= 0 and row + r < row_max and col + c >= 0 and col + c < col_max:
-                if grid[row+r][col+c]:
+                # print('row value being checked')
+                # print(row+r)
+                # print('col value being checked')
+                # print(col+c)
+                if grid[row+r][col+c] and not visited[row+r][col+c]:
                     valid_neighbors.append((row+r, col+c))
-        return valid_neighbors
+                    # print('location to be updated row: {} and col: {}'.format(row + r, col + c))
+                    # x = row+r
+                    # y = col+c
+                    # print('x', x)
+                    # print('y', y)
+                    visited[row+r][col+c] = True
+                    # print('--- in function call----')
+                    # display_grid(visited)
+                    # print('--- after function call----')
+        return valid_neighbors, visited
 
 
     # def is_valid_input(start_row_idx, start_col_idx , end_row_idx, end_col_idx):
@@ -70,7 +86,7 @@ def path_exists(grid, queries):
     #         or end_row_idx < 0 or end_row_idx >= rows or :
 
     if queries:
-        print(len(queries))
+        # print(len(queries))
         result = [False] * len(queries)
     else:
         return []
@@ -82,44 +98,75 @@ def path_exists(grid, queries):
     row_max = len(grid)
     col_max = len(grid[0])
 
-    visited = [[False] * col_max] * row_max
+
 
     queue = []
 
     for i, query in enumerate(queries):
         start_row_idx = query[0][0]
         start_col_idx = query[0][1]
-        end_row_idx = query[0][0]
-        end_col_idx = query[0][1]
+        end_row_idx = query[1][0]
+        end_col_idx = query[1][1]
+
+        ## Validate the query input
+        #
+        # if start_row_idx < 0 or start_row_idx >= row_max or \
+        #     start_col_idx < 0 or start_col_idx >= col_max or \
+        #     end_row_idx < 0 or end_row_idx >= row_max or \
+        #     end_col_idx < 0 or end_col_idx >= col_max:
+        #
+        #     raise ValueError("Invalid indexes provided in query input")
+
+
+        # print(start_row_idx)
+        # print(start_col_idx)
+
+        visited = [[False for _ in range(col_max)] for _ in range(col_max)]
+
 
         ## If the value at start-index or value at end-index is not 1
-        if not grid[start_row_idx][start_col_idx] and not grid[end_row_idx][end_col_idx]:
-            # result.append(False)
-            result[i] = False
-            break
+        try:
+            if not grid[start_row_idx][start_col_idx] or not grid[end_row_idx][end_col_idx]:
+                # result.append(False)
+                # print('not valid')
+                continue
+        except IndexError:
+            print("Invalid indexes passed in query number: {} \ninput value {}".format(i, query))
+            continue
 
+        # print('still going on')
         queue.append((start_row_idx, start_col_idx))
 
         while queue:
-            node = queue.pop()
+            # print('queue', queue)
+            node = queue.pop(0)
+            # print('popped node', node)
+            # print('pop queue', queue)
+            # print('node 0: ', node[0])
+            # print(end_row_idx)
+            # print('node 1: ', node[1])
+            # print(end_col_idx)
             if node[0] == end_row_idx and node[1] == end_col_idx:
-                # result.append(True)
+                # print('result found!!')
                 result[i] = True
                 break
-            valid_neighbors = check_valid_neighbors(node[0], node[1], row_max, col_max)
-            if not valid_neighbors:
-                # result.append(False)
-                result[i] = False
-                break
-            else:
-                queue.extend(valid_neighbors)
-        result[i] = False
+            visited[node[0]][node[1]] = True
+            valid_neighbors, visited = check_valid_neighbors(node[0], node[1], row_max, col_max, visited)
+            # display_grid(visited)
+            # if valid_neighbors:
+                # print('list extended')
+            queue.extend(valid_neighbors)
+            # else:
+            #     print('empty neighbors')
         # result.append(False)
+        # print('after processing')
+        # print(visited)
     return result
     # raise NotImplementedError
 
 
-queries = [((0, 0), (6, 6)), ((1, 1), (4, 4)), ((0,), (4, 4)), ((0, 0), (0, 4)), ((0, 0), (6, 3))]
+queries = [((-1, 1), (4, 8)), ((1, 1), (4, 4)), ((0, 1), (4, 4))]
+# queries = [((0, 0), (6, 6)), ((1, 1), (4, 4)), ((0, 1), (4, 4)), ((0, 0), (0, 4)), ((0, 0), (6, 3))]
 grid = [[1, 1, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 0, 1, 0],
         [1, 1, 0, 1, 0, 1, 1],
